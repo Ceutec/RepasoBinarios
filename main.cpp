d4a82007161c969cc88c1d79717f37fd739eec25
Turupawn <turupawn@userr>
2014-10-16 08:43:44 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int opcion;
+    do
+    {
+        cout<<"Menu"<<endl;
+        cout<<"1. Abrir"<<endl;
+        cout<<"2. Imprimir"<<endl;
+        cout<<"3. Agregar"<<endl;
+        cout<<"4. Modificar"<<endl;z
+        cout<<"5. Salir"<<endl;
+        cout<<"Opcion: ";
+        cin>>opcion;
+    }while(opcion!=5);
+
+
+    return 0;
+}
